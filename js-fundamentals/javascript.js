fbf158e80c7d241cfaef77ea9fa386070e805be0
Joseph Williams <josewilliams@meta.com>
2023-02-24 16:08:27 -0500

    Complete the assignment for fundamentals part 1

---
+++ js-fundamentals/javascript.js
@@ -0,0 +1,18 @@
+// console.log(23+97);
+// console.log(1+2+3+4+5+6);
+// console.log((4+6+9)/77);
+
+// let a = 10;
+// console.log(10);
+// console.log(9*a);
+
+// let b = 7 * a;
+// console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+
+console.log(max);
+console.log(actual);
+console.log(percentage);
