9833436976ac6839bd427a877360a200d98028d6
xJWLLMSx <97003350+xjwllmsx@users.noreply.github.com>
2023-02-27 23:59:11 -0500

    Add code from 'Check the login' exercise

--- js-fundamentals/javascript.js
+++ js-fundamentals/javascript.js
@@ -9,10 +9,29 @@
 // let b = 7 * a;
 // console.log(b);
 
-const max = 57;
-const actual = max - 13;
-const percentage = actual / max;
+// const max = 57;
+// const actual = max - 13;
+// const percentage = actual / max;
 
-console.log(max);
-console.log(actual);
-console.log(percentage);
+// console.log(max);
+// console.log(actual);
+// console.log(percentage);
+
+// Check the login exercise found on https://javascript.info/task/check-login
+let userName = prompt(`Who's there?`);
+
+if(userName === 'Admin') {
+    let pass = prompt(`Password?`);
+
+    if (pass === `TheMaster`) {
+        alert(`Welcome!`);
+    } else if (pass === '' || pass === null) {
+        alert(`Canceled`);
+    } else {
+        alert(`Wrong password`);
+    }
+} else if (userName === `` || userName === null) {
+    alert(`Canceled`);
+} else {
+    alert(`I don't know you`);
+}
